51eebf2bcd5615c2802f12293d8375c3bde0da5e
grSashha <90776548+grSashha@users.noreply.github.com>
2022-03-30 21:46:42 +0300

    Сделал lab4

--- lab4/1.js
+++ lab4/1.js
@@ -6,7 +6,42 @@
  * ['мир', 'Рим', 'сирота', 'Ариост', 'мри', 'пва', 'лор', 'авп']; -> [["мир", "Рим", "мри"], ["сирота", "Ариост"], ["пва", "авп"]]
  */
 function getAnagramms(arr) {
-    //code here
+    let copy_arr = arr.slice(0, arr.length);
+    let res = [];
+    for (let i = 0; i < copy_arr.length; i++) {
+       let temp = [];
+       temp.push(copy_arr[i]);
+       let arr_copy_str2;
+       let copy_str2;
+       for (let j = i + 1; j < copy_arr.length; j++) {
+           if (copy_arr[i].length === copy_arr[j].length) {
+                let str1 = copy_arr[i].toLowerCase();
+                let str2 = copy_arr[j].toLowerCase();
+                copy_str2 = str2;
+                //---------------------------------------
+                for (let k = 0; k < str1.length; k++) {
+                    for (let s = 0; s < str1.length; s++) {
+                        if (str1[k] === str2[s]) {
+                            copy_str2 = copy_str2.replace(str2[s], '_');
+                        }
+                        else continue;
+                    }
+                }
+                //---------------------------------------
+            }
+            else continue;
+            arr_copy_str2 = copy_str2.split('');
+            if (arr_copy_str2.every(elem => elem === '_') && copy_arr[j].length > 0) temp.push(copy_arr[j]);
+        }
+        if (temp.length > 0 && temp[0].length > 0) res.push(temp);
+        for (let m = 0; m < temp.length; m++)
+            for (let b = 0; b < copy_arr.length; b++)
+                if (temp[m] === copy_arr[b]) {
+                    delete copy_arr[b];
+                    copy_arr[b] = "";
+                }
+    }
+    return res;
 }
 
 module.exports = getAnagramms;
--- lab4/5.js
+++ lab4/5.js
@@ -10,8 +10,62 @@
  * [(<>)] --> true
  */
 
+ function fn(ch, obj_arg) {
+    let res = true;
+    for (let e in obj_arg) {
+        if (ch === e) {
+            res = true;
+            return res;
+            break;
+        }
+    }
+    for (let e in obj_arg) {
+        if (ch === obj_arg[e]) {
+            res = false;
+            return res;
+            break;
+        }
+    }
+    return res;
+}
+
 function checkBrackets(str) {
-    //code here
+    let res = true, arr = [];
+    let obj = {'[': ']', '{': '}', '<': '>', '(': ')'};
+    for (let i = 0; i < str.length; i++)
+        for (let e in obj)
+            if ((str[i] === obj[e]) || (str[i] === e)) arr.push(str[i]);
+    if (!(fn(arr[0], obj))) res = false;
+
+    if (res === false) return res;
+    else {
+        for (let k = 0; k < arr.length; k++) {
+            if (fn(arr[k], obj)) {
+                let m = 0, b = k;
+                for (let j = k+1; j < arr.length; j++) {
+                    let a = j;
+                    if (fn(arr[j], obj)) {
+                        m++;
+                        continue;
+                    }
+                    else if (m === 0) {
+                        if (obj[arr[k]] === arr[j]) {
+                            arr[j] = '_';
+                            arr[k]= '_';
+                        }
+                        break;
+                    }
+                    else m--;
+                }
+            }
+        }
+    }
+    let flag = true;
+    arr.forEach((element) => {
+        if (element != '_') flag = false;
+    });
+    if (arr.length === 0) return true;
+    else return flag;
 }
 
 module.exports = checkBrackets;
